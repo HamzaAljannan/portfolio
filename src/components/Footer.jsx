a0450028a863b9e001a9f3d5544aab02f72da29d
hamza aljannan <hamza.aljannan.dev@gmail.com>
2025-07-31 18:01:36 +0300

    Edit name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='max-w-[1300px] mx-auto flex justify-between p-6 md:p-20 text-sm md:text-lg mt-12'>
         <div className='space-y-4'>
-            <h3 className='text-2xl text-gray-200 font-semibold'>Hamza Aljannan</h3>
+            <h3 className='text-2xl text-gray-200 font-semibold'>Hamza Al Jannan</h3>
             <div className='flex flex-row gap-6 text-gray-400 text-4xl'>
                 <a href="https://github.com/HamzaAljannan"><FaGithubSquare /></a>
                 
@@ -13,7 +13,7 @@ const Footer = () => {
 
         </div>
 
-        <p className='text-gray-400'>@2025 Hamza Aljannan</p>
+        <p className='text-gray-400'>@2025 Hamza Al Jannan</p>
         
     </div>
   )
